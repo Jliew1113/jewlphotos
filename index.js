2ed3fdc4fc85122192389d62fd158c2cd7215171
Jliew1113-Desktop <shanmou@rocketmail.com>
2023-02-28 14:27:51 +1100

    vue

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
 var app = new Vue({
-    el: '#navbar',
+    el: 'navbar',
     data: {
         isCollapsed: false
     }, 
 });
 
-var app = new Vue({
-    el: '#slideshow',
-    data: {
-        images: [
-            { src: './res/IMG_7341.jpg', alt: 'Image 1' },
-            { src: './res/IMG_7341.jpg', alt: 'Image 2' }
-        ],
-        currentIndex: 0
-    },
-    computed: {
-        currentImage: function() {
-         return this.images[this.currentIndex];
-        }
-    },
-    methods: {
-        nextImage: function() {
-            this.currentIndex = (this.currentIndex + 1) % this.images.length;
-        }
-    }
-});
