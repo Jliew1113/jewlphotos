c273e4657a8cb9534fc7ddfe5081b9955ebc643d
Jliew1113 <73236403+Jliew1113@users.noreply.github.com>
2023-03-08 11:22:45 +1100

    Menu

--- js/main.js
+++ js/main.js
@@ -195,7 +195,7 @@
 			var header = $('#fh5co-header'),
 				scrlTop = $(this).scrollTop();
 	
-			if ( scrlTop > 500 && scrlTop <= 2000 ) {
+			if ( scrlTop > 0 && scrlTop <= 2000 ) {
 				header.addClass('navbar-fixed-top fh5co-animated slideInDown');
 			} else if ( scrlTop <= 500) {
 				if ( header.hasClass('navbar-fixed-top') ) {
